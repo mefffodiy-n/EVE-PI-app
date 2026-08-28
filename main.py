181bb36ab99698605b643c230b3cc8362510a478
Mikhail Nedbailo <126399598+mefffodiy-n@users.noreply.github.com>
2026-08-29 00:54:56 +0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
